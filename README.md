cc6541a1332256607a02662651df208550ad8b28
Rarug <frarug@provart.com.ar>
2023-09-11 12:57:11 -0300

    WIP boton de habilidades

--- README.md
+++ README.md
@@ -15,19 +15,25 @@ Una aplicación sencilla que consume la API de Pokémon para mostrarte informaci
 
 2. **Navega hasta el directorio del proyecto:**:
 
+    ```bash
     cd [pokemon-app]
 
 3. **Instalar las dependencias**:
 
 Ejecuta el siguiente comando para instalar todas las dependencias del proyecto:
-**npm install**
 
+```bash
+npm install
+```
 ## 🚀 Ejecución del proyecto
 
 **Ejecutar la aplicación:**:
 
 Una vez que hayas completado los pasos de instalación, puedes ejecutar la aplicación con el siguiente comando:
-**npm start**
+
+```bash
+npm start
+```
 
 Esto iniciará la aplicación y, si todo funciona correctamente, deberías ver un mensaje en la consola que indica que la aplicación está corriendo y en qué puerto.
 
--- src/pokemonSearch.js
+++ src/pokemonSearch.js
@@ -5,9 +5,11 @@ function PokemonSearch() {
     const [pokemon, setPokemon] = useState(null);
     const [errorMsg, setErrorMsg] = useState('');
     const [loading, setLoading] = useState(false);
+    const [showDetails, setShowDetails] = useState(false);
 
     const fetchPokemon = async () => {
-        setLoading(true); 
+        setLoading(true);
+        setShowDetails(false);
 
         try {
             const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`);
@@ -18,39 +20,82 @@ function PokemonSearch() {
 
             const data = await response.json();
             setPokemon(data);
-            setErrorMsg(''); 
+            setErrorMsg('');
         } catch (error) {
             setPokemon(null);
             setErrorMsg(error.message);
         } finally {
-            setLoading(false); 
+            setLoading(false);
         }
     };
 
     return (
         <div>
-            <input 
-                type="number" 
-                placeholder="Número de Pokedex" 
-                value={pokeId}
-                onChange={(e) => setPokeId(e.target.value)}
-            />
-            <button onClick={fetchPokemon}>Buscar</button>
-
+            <div>
+                <input 
+                    type="number" 
+                    placeholder="Número de Pokedex" 
+                    value={pokeId}
+                    onChange={(e) => setPokeId(e.target.value)}
+                />
+                <button onClick={fetchPokemon}>Buscar</button>
+            </div>
+    
             {loading && <div className="loading-bar"></div>}
-
-
+    
             {errorMsg && <p className="error">{errorMsg}</p>}
+    
             {pokemon && (
-                <div>
+                <div className="pokemon-card">
                     <h2>{pokemon.name}</h2>
                     <img src={pokemon.sprites.front_default} alt={pokemon.name} />
                 </div>
             )}
+    
+            {pokemon && (
+            <div>
+                <button className="show-details-btn" onClick={() => setShowDetails(!showDetails)}>
+                    {showDetails ? "Ocultar detalles" : "Ver detalles"}
+                </button>
+            </div>
+            )}
+
+    
+            {showDetails && (
+                <div className="details-card">
+                    <h3>Características</h3>
+                    <div className="detail">
+                        <strong>Peso:</strong> {pokemon.weight / 10} kg
+                    </div>
+                    <div className="detail">
+                        <strong>Altura:</strong> {pokemon.height / 10} m
+                    </div>
+                    
+                    <h3>Tipo</h3>
+                    <div className="detail">
+                        {pokemon.types.map((typeObj, index) => (
+                            <span key={index}>{typeObj.type.name} </span>
+                        ))}
+                    </div>
+
+                    <h3>Habilidades</h3>
+                    {pokemon.abilities.map((abilityObj, index) => (
+                        <div className="detail" key={index}>
+                            {abilityObj.ability.name}
+                        </div>
+                    ))}
+                </div>
+            )}
+
+
         </div>
     );
+    
 }
 
 export default PokemonSearch;
+
+
+
 
 
